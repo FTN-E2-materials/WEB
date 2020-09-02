678a83f60b0528e9f420c93844f7e8d7944d14ae
Jelena Vlajkov <vlajkovj31@gmail.com>
2020-09-02 18:13:25 +0200

    aaaaaaaa

--- BookingApp/static/js/add_apartment.js
+++ BookingApp/static/js/add_apartment.js
@@ -15,54 +15,61 @@ Vue.component("add_apartment", {
     },
 
     template: `
-    <div class="form-part" v-bind:hidden="next==true">
+    <div>
+
+
+    <div v-bind:hidden="next==true">
+    <div class="form-part" >
     <h2>Dodajte novi apartman</h2>
     <div class="form-apt" v-bind:hidden="next==true">
         <div class="col-add">
         <p class = info-apt>Naziv</p>
-        <input type="text" id="apt-name" class = "details" v-model="apartmentName">
+        <input type="text" class = "details" v-model="apartmentName">
         <p class = "info-apt">Tip apartmana</p>
         <div class = "col-type-radio">
             <div class = "col-date">
-                <input type="radio" id="type-apt" class = "radio-apt" name="sortbydate" value="Najnovije" v-model="apartmentType">
+                <input type="radio" class = "radio-apt" name="sortbydate" value="Najnovije" v-model="apartmentType">
                 <p class = "sortbydate-font">Soba</p>
             </div>
             <div class = "col-date">
-                <input type="radio" id="type-apt" class = "radio-apt" name="sortbydate" value="Najstarije" v-model="apartmentType">
+                <input type="radio" class = "radio-apt" name="sortbydate" value="Najstarije" v-model="apartmentType">
                 <p class = "sortbydate-font">Ceo apartman</p>
             </div>
         </div>
         <p class = info-apt>Broj gostiju</p>
-        <input type="number" id="apt-num" class = "details" v-model="guestsNumber">
+        <input type="number" class = "details" v-model="guestsNumber">
         <p class = info-apt>Država</p>
-        <input type="text" id="apt-num" class = "details" v-model="country" >
+        <input type="text"  class = "details" v-model="country" >
         <p class = info-apt>Mesto</p>
-        <input type="text" id="apt-num" class = "details" v-model="state">
+        <input type="text" class = "details" v-model="state">
         </div>
         <div class="col-add">
         <p class = info-apt>Cena</p>
-        <input type="text" id="apt-name" class = "details" v-model="price">
+        <input type="text" class = "details" v-model="price">
         <p class = "info-apt">Valuta</p>
         <div class = "col-type-radio">
             <div class = "col-date">
-                <input type="radio" id="type-apt" class = "radio-apt" name="valuta" value="Najnovije" v-model="currency">
+                <input type="radio" class = "radio-apt" name="valuta" value="Najnovije" v-model="currency">
                 <p class = "sortbydate-font">RSD</p>
             </div>
             <div class = "col-date">
-                <input type="radio" id="type-apt" class = "radio-apt" name="valuta" value="Najstarije" v-model="currency">
+                <input type="radio" class = "radio-apt" name="valuta" value="Najstarije" v-model="currency">
                 <p class = "sortbydate-font">EUR</p>
             </div>
         </div>            
     
         <p class = "info-apt">Vreme prijave</p>
-        <input type="text" id="apt-num" class = "details" v-model="applicationTime">
+        <input type="text" class = "details" v-model="applicationTime">
         <p class = "info-apt">Vreme odjave</p>
-        <input type="text" id="apt-num" class = "details" v-model="checkOutTime">
+        <input type="text" class = "details" v-model="checkOutTime">
         </div>
-        <button type="button" class="submit-apt" v-on:click="NextButton" v-bind:hidden="next==true" >Dalje</button> 
+        <div v-bind:hidden = "next == true">
+        <button type="button" class="submit-apt" v-on:click="NextButton" >Dalje</button>
+        </div> 
     </div>
-
-    <div class="form-part-pics" v-bind:hidden="next==true">
+    </div>
+    </div>
+    <div class="form-part-pics" v-bind:hidden="next==false">
     <div class="apt-images">
         <p class="info-apt">Oznacite fotografije apartmana:</p>
         <input type="file" class="details">
@@ -72,16 +79,20 @@ Vue.component("add_apartment", {
         <input type="text" class="details">
         <button class="submit">Dodaj</button>
     </div>
-    </div>
-</div> ` ,
+
+</div> 
+</div>` ,
 
     mounted () {
+        this.next = false;
+        console.log(this.next);
 
     },
     methods : {
        NextButton : function()
        {
            this.next=true;
+           console.log(this.next);
        }
     }
 });
